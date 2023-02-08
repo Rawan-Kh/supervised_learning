58c012cbaa399e0b3a3d74886402244fe8df04be
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-08 21:10:52 +0200

    Update chapter2.py

--- chapter2.py
+++ chapter2.py
@@ -89,3 +89,46 @@ cv_scores = cross_val_score(reg, X, y, cv=kf)
 print(cv_scores)
 
 --------------------
+# Print the mean
+print(np.mean(cv_results))
+
+# Print the standard deviation
+print(np.std(cv_results))
+
+# Print the 95% confidence interval
+print(np.quantile(cv_results, [0.025, 0.975]))
+------------
+
+# Import Ridge
+from sklearn.linear_model import Ridge
+alphas = [0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
+ridge_scores = []
+for alpha in alphas:
+  
+  # Create a Ridge regression model
+  ridge = Ridge(alpha=alpha)
+  
+  # Fit the data
+  ridge.fit(X_train, y_train)
+  
+  # Obtain R-squared
+  score = ridge.score(X_test, y_test)
+  ridge_scores.append(score)
+print(ridge_scores)
+
+-------------------
+# Import Lasso
+from sklearn.linear_model  import Lasso
+
+# Instantiate a lasso regression model
+lasso = Lasso(alpha= 0.3)
+
+# Fit the model to the data
+lasso.fit(X, y)
+
+# Compute and print the coefficients
+lasso_coef = lasso.fit(X, y).coef_
+print(lasso_coef)
+plt.bar(sales_columns, lasso_coef)
+plt.xticks(rotation=45)
+plt.show()
